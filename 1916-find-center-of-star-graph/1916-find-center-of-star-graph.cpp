a7d8b63f4398e398771dbc277cff412f57ae2bc6
Sarvesh Sugandh Barnwal <sarveshb259@gmail.com>
2023-09-13 07:36:37 +0530

    Time: 146 ms (88.77%), Space: 67.3 MB (75.23%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
-    int findCenter(vector<vector<int>>& edges) {
-        vector<int> e(100001,0);
-        int maxi=-1,freq=-1;
-        for(auto i:edges)
-        {
-            e[i[0]]++;
-            if(e[i[0]]>freq)
-            {
-                maxi=i[0];
-                freq=e[i[0]];
-            }
-            e[i[1]]++;
-            if(e[i[1]]>freq)
-            {
-                maxi=i[1];
-                freq=e[i[1]];
-            }
-        }
-        return maxi;
+    int findCenter(vector<vector<int>>& i) {
+        if(i[0][0]==i[1][0])
+            return i[0][0];
+        if(i[0][0]==i[1][1])
+            return i[1][1];
+        return i[0][1];
     }
 };
