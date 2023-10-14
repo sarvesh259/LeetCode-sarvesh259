eee004f9efa21a80e0c7f50fede100517e0bbce6
Sarvesh Sugandh Barnwal <sarveshb259@gmail.com>
2023-10-14 12:29:46 +0530

    Time: 11 ms (23.63%), Space: 10.5 MB (14.59%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -3,29 +3,24 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
         vector<vector<int>> dp(m,vector<int>(n,0));
-        dp[0][0]=grid[0][0];
-        for(int i=1;i<m;i++)
-            dp[i][0]=grid[i][0]+dp[i-1][0];
-        for(int i=1;i<n;i++)
-            dp[0][i]=grid[0][i]+dp[0][i-1];
-        for(int i=1;i<m;i++)
+        for(int i=0;i<m;i++)
         {
-            for(int j=1;j<n;j++)
+            for(int j=0;j<n;j++)
             {
-                int up=0,left=0;
-                if(i>0)
-                    up=dp[i-1][j];
-                if(j>0)
-                    left=dp[i][j-1];
-                dp[i][j]=min(up,left)+grid[i][j];
+                if(i==0&&j==0)  dp[i][j]=grid[0][0];
+                else
+                {
+                    int up=grid[i][j],left=grid[i][j];
+                    if(i>0)
+                        up+=dp[i-1][j];
+                    else up=1e9;
+                    if(j>0)
+                        left+=dp[i][j-1];
+                    else left=1e9;
+                    dp[i][j]=min(up,left);
+                }
             }
         }
-        // for(auto i:dp)
-        // {
-        //     for(auto j:i)
-        //         cout<<j<<" ";
-        //     cout<<endl;
-        // }
         return dp[m-1][n-1];
     }
 };
