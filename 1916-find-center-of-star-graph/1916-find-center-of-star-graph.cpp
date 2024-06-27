9d33a46ef644c384251c2d5155e8252cb7b438e5
Sarvesh Sugandh Barnwal <sarveshb259@gmail.com>
2024-06-27 23:48:32 +0530

    Time: 136 ms (61.47%), Space: 70.8 MB (54.65%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& i) {
-        if(i[0][0]==i[1][0])
-            return i[0][0];
-        if(i[0][0]==i[1][1])
-            return i[1][1];
-        return i[0][1];
+        unordered_set<int> st;
+        int size=0;
+        st.insert(i[0][0]);
+        st.insert(i[0][1]);
+        st.insert(i[1][0]);
+        return st.size()==2?i[1][0]:i[1][1];
     }
 };
