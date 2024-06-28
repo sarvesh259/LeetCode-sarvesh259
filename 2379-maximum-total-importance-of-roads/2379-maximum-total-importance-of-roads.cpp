0ebaf598a20e4faaa0314203058c5caa807f80c0
Sarvesh Sugandh Barnwal <sarveshb259@gmail.com>
2024-06-29 00:10:45 +0530

    Time: 306 ms (53.97%), Space: 132.8 MB (57.93%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        vector<int> degree(n, 0); // Array to store the degree of each city
-        
-        // Calculate the degree of each city
-        for (const auto& road : roads) {
-            degree[road[0]]++;
-            degree[road[1]]++;
+        vector<int> a(n,0);
+        for(auto i:roads)
+        {
+            a[i[0]]++;
+            a[i[1]]++;
         }
-        
-        // Create a list of cities and sort by degree
-        vector<int> cities(n);
-        for (int i = 0; i < n; i++) {
-            cities[i] = i;
+        sort(a.begin(),a.end(),greater<int>());
+        long long ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans+=(n-i)*1LL*a[i];
         }
-        sort(cities.begin(), cities.end(), [&](int a, int b) {
-            return degree[a] > degree[b];
-        });
-        
-        // Assign values to cities starting from the highest degree
-        long long totalImportance = 0;
-        for (int i = 0; i < n; i++) {
-            totalImportance += (long long)(n - i) * degree[cities[i]];
-        }
-        
-        return totalImportance;
+        return ans;
     }
 };
