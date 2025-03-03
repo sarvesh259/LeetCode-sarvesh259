ad3b3db2cbebe074702862cc1fd6f5f1c85f4112
Sarvesh Sugandh Barnwal <sarveshb259@gmail.com>
2025-03-03 23:17:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,49 +1,56 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int colSize=matrix.size(),rowSize=matrix[0].size(),col1=matrix[0][0];
-        for(int i=0;i<colSize;i++)
+        int temp=1;
+        for(int i=0;i<matrix.size();i++)
         {
-            for(int j=0;j<rowSize;j++)
+            for(int j=0;j<matrix[0].size();j++)
             {
                 if(matrix[i][j]==0)
                 {
-                    matrix[i][0]=0;
-                    if(j==0)
+                    if(i==0)
                     {
-                        col1=0;
+                        temp=0;
                     }
                     else
                     {
-                        matrix[0][j]=0;
+                        matrix[i][0]=0;
                     }
-
+                    matrix[0][j]=0;
                 }
             }
         }
-        for(int i=1;i<colSize;i++)
-        {   
-            for(int j=1;j<rowSize;j++)
+        for(int i=1;i<matrix[0].size();i++)
+        {
+            if(matrix[0][i]==0)
             {
-                if(matrix[i][0]==0||matrix[0][j]==0)
+                for(int j=0;j<matrix.size();j++)
+                {
+                    matrix[j][i]=0;
+                }
+            }
+        }
+        for(int i=0;i<matrix.size();i++)
+        {
+            if(matrix[i][0]==0)
+            {
+                for(int j=0;j<matrix[0].size();j++)
                 {
                     matrix[i][j]=0;
                 }
             }
         }
-        if(matrix[0][0]==0)
+        if(temp==0)
         {
-            for(int i=1;i<rowSize;i++)
-            {
-                matrix[0][i]=0;
-            }
+            for(int j=0;j<matrix[0].size();j++)
+                {
+                    matrix[0][j]=0;
+                }
+                for(int j=0;j<matrix.size();j++)
+                {
+                    matrix[j][0]=0;
+                }
         }
-        if(col1==0)
-        {
-            for(int i=0;i<colSize;i++)
-            {
-                matrix[i][0]=0;
-            }
-        }
+        // return matrix;
     }
 };
