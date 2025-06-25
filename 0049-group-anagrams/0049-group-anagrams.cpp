8d82157abaa1de663038d66287f8b54bb61010c3
Sarvesh Sugandh Barnwal <sarveshb259@gmail.com>
2025-06-25 19:31:53 +0530

    Time: 86 ms (5.51%), Space: 34.6 MB (8.81%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-         unordered_map<string,vector<string>> mp;
+         map<string,vector<string>> mp;
         for(auto &i:strs)
         {
             vector<int> hash(26,0);
@@ -10,14 +10,14 @@ public:
                 hash[j-'a']++;
             }
             string temp="";
-            for(auto&j:hash)
+            for(int j=0;j<26;j++)
             {
-                temp+=to_string(j)+",";
+                temp+=to_string(hash[j])+",";
             }
             mp[temp].push_back(i);
         }
         vector<vector<string>> ans;
-        for(auto i:mp)
+        for(auto &i:mp)
         {
             ans.push_back(i.second);
         }
