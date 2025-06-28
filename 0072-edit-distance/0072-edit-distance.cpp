523833dde9db7c7e8e0e5f5300f7f282b85f40a7
Sarvesh Sugandh Barnwal <sarveshb259@gmail.com>
2025-06-28 18:20:44 +0530

    Time: 3 ms (90.41%), Space: 10.3 MB (97.53%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    int findAns(int i,int j,string &s1,string &s2,vector<vector<int>> &dp)
-    {
-        if(i==-1) return j+1;
-        if(j==-1) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1[i]==s2[j]) return dp[i][j]=findAns(i-1,j-1,s1,s2,dp);
-        return dp[i][j]= 1+min({findAns(i-1,j,s1,s2,dp),findAns(i-1,j-1,s1,s2,dp),findAns(i,j-1,s1,s2,dp)});
-    }
     int minDistance(string s1, string s2) {
-        vector<vector<int>> dp(s1.size(),vector<int>(s2.size(),-1));
-        return findAns(s1.size()-1,s2.size()-1,s1,s2,dp);
+        // vector<vector<int>> dp(s1.size(), vector<int>(s2.size(), 0));
+        vector<int> cur(s2.size()+1,0);
+        vector<int> prev(s2.size()+1,0);
+        for(int i=0;i<=s2.size();i++) prev[i]=i;
+        for (int i = 1; i <= s1.size(); i++) {
+            cur[0]=i;
+            for (int j = 1; j <= s2.size(); j++) {
+                if (s1[i-1] == s2[j-1])
+                    cur[j] = prev[j - 1];
+                else
+                    cur[j]= 1+min(prev[j],min(prev[j-1],cur[j-1]));
+            }
+            prev=cur;
+        }
+        return prev[s2.size()];
     }
 };
