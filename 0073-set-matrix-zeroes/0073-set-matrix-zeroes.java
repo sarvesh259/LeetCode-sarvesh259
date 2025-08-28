bc445abd8cbc16a808120868c4f3d189c1f5f53c
Sarvesh Sugandh Barnwal <sarveshb259@gmail.com>
2025-08-28 23:02:58 +0530

    Time: 2 ms (17.9%), Space: 45.5 MB (94.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,40 +1,35 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int[]row = new int[m];
-        int[]col = new int[n];
-        Arrays.fill(row,-1);
-        Arrays.fill(col,-1);
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-               if(matrix[i][j]==0)
-               {
-                    row[i]=0;
-                    col[j]=0;
-               } 
-            }
-        }
-        for(int i=0;i<m;i++)
-        {
-            if(row[i]==0)
-            {
-                for(int j=0;j<n;j++)
-                {
-                    matrix[i][j]=0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int temp = -1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        temp = 0;
+                    }
                 }
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            if(col[i]==0)
-            {
-                for(int j=0;j<m;j++)
-                {
-                    matrix[j][i]=0;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] != 0 && (matrix[i][0] == 0 || matrix[0][j] == 0)) {
+                    matrix[i][j] = 0;
                 }
+            }
+        }
+        if (matrix[0][0] == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if (temp == 0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
