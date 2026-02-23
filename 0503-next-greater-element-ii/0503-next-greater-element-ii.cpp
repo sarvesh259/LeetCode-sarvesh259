bf3d66aa11fd724eb50607cbd894c0a3d698ea91
Sarvesh Sugandh Barnwal <sarveshb259@gmail.com>
2026-02-24 01:52:01 +0530

    Time: 0 ms (100%), Space: 28.9 MB (17.99%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int maxEleIndex=max_element(nums.begin(),nums.end())-nums.begin();
+        // int maxEleIndex=max_element(nums.begin(),nums.end())-nums.begin();
         int n=nums.size();
         vector<int> ans(n,-1);
         stack<int> st;
-        for(int i=maxEleIndex;i>=0;i--)
+        for(int i=n-1;i>=0;i--)
+        {
+            st.push(nums[i]);
+        }
+        for(int i=n-1;i>=0;i--)
         {
             while(!st.empty()&&st.top()<=nums[i])
             {
@@ -17,18 +21,18 @@ public:
             }
             st.push(nums[i]);
         }
-        for(int i=n-1;i>maxEleIndex;i--)
-        {
-            while(!st.empty()&&st.top()<=nums[i])
-            {
-                st.pop();
-            }
-            if(!st.empty())
-            {
-                ans[i]=st.top();
-            }
-            st.push(nums[i]);
-        }
+        // for(int i=n-1;i>maxEleIndex;i--)
+        // {
+        //     while(!st.empty()&&st.top()<=nums[i])
+        //     {
+        //         st.pop();
+        //     }
+        //     if(!st.empty())
+        //     {
+        //         ans[i]=st.top();
+        //     }
+        //     st.push(nums[i]);
+        // }
         return ans;
     }
 };
